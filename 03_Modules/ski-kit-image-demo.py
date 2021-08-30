08043b954186e92ac6bd0574021ce95c653a6cd7
Lenovo <dieter.vanderelst@fastmail.net>
2021-08-30 16:42:50 -0400

    prepare class

---
+++ 03_Modules/ski-kit-image-demo.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 30 13:31:08 2021
+
+@author: dieter
+"""
+
+from skimage import data
---
+++ class_code/simple_weather_plot.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import turtle			
+import requests
+from matplotlib import pyplot
+
+api_key = 'eb85b0a4df49360254aad7a547a0de9b'
+
+lat = '39'
+lon = '-84'
+url = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&appid="+api_key
+
+
+# Get the data from the web
+response = requests.get(url) 
+data = response.json()
+
+
+# create list with selected data
+hourly = data['hourly']
+selected_data = []
+for data_point in hourly:
+    selected_data.append(data_point['temp'])
+
+
+
+pyplot.plot(selected_data)

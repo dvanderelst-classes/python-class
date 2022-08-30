d37c7bfb65e616af9de1e298c910d3bb05e51c96
Lenovo <dieter.vanderelst@fastmail.net>
2022-08-30 15:09:54 -0400

    class code updated

---
+++ class_code/my_first_script.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 30 14:58:19 2022
+
+@author: dieter
+
+This script is used to hack the nasa
+
+"""
+
+
+
+side = 7
+
+# here I calc the surf area of square
+area = side * side
+circumference = side * 4
+volume = side**3
+
+# Here ir print output
+print(side)
+print(area)
+print(circumference)
+print(volume)
